408d9abbc1fd2bada32165e7007f3e0ea30763e7
teddy <teddywambua5456@gmail.com>
2023-06-29 11:05:36 +0300

    Completed assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -13,20 +14,24 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
-
-  function handleSubmit(e) {
+  
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
-
+  
   function handleChange(e) {
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
@@ -125,33 +130,17 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
   );
 }
-
-const Wrapper = styled.section`
-  max-width: 500px;
-  margin: 32px auto;
-  padding: 32px;
-`;
-
-const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 16px;
-`;
-
-const SubmitButton = styled.button`
-  background: blue;
-  color: yellow;
-  font-weight: bold;
-  font-family: inherit;
-  font-size: 1.2rem;
-  border: none;
-  padding: 8px 16px;
-  cursor: pointer;
-`;
 
 export default MovieForm;
